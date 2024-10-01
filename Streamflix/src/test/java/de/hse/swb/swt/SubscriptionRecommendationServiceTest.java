a8cfbea5e7690ae653bd84e78318840285e881df
danGitRys <daniel@familie-ryssel.de>
2024-10-01 19:37:42 +0200

    created dummy test

---
+++ Streamflix/src/test/java/de/hse/swb/swt/SubscriptionRecommendationServiceTest.java
@@ -0,0 +1,16 @@
+package de.hse.swb.swt;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+public class SubscriptionRecommendationServiceTest {
+    
+    /**
+     * Dummy Test to test if Junit works
+     */
+
+    @Test
+    void dummyTest(){
+        assertEquals(1,1);
+    }
+
+}
