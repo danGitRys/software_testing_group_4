4af9e899d9831647f7c660646edef9cd19afafdc
danGitRys <daniel@familie-ryssel.de>
2024-10-01 19:49:09 +0200

    removed dummy test and added one test per subscriptionmodel

--- Streamflix/src/test/java/de/hse/swb/swt/SubscriptionRecommendationServiceTest.java
+++ Streamflix/src/test/java/de/hse/swb/swt/SubscriptionRecommendationServiceTest.java
@@ -2,15 +2,35 @@ package de.hse.swb.swt;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
+import de.hse.swb.swt.streamflix.SubscriptionRecommendationService;
 public class SubscriptionRecommendationServiceTest {
     
     /**
      * Dummy Test to test if Junit works
      */
 
+    
+
     @Test
-    void dummyTest(){
-        assertEquals(1,1);
+    void  testStandardSubscriptionWithAds(){
+        assertEquals("W",SubscriptionRecommendationService.recommendModel(false, 0, false, 0));
+    }
+
+    
+    @Test
+    void testBasicSubscripton(){
+        assertEquals("B",SubscriptionRecommendationService.recommendModel(true, 2, true, 1));
+    }
+
+    @Test
+    void testStandardSubscription(){
+        assertEquals("S", SubscriptionRecommendationService.recommendModel(true, 2, true, 2));
+    }
+
+    @Test
+    void testPremiumSubscription(){
+        assertEquals("P",SubscriptionRecommendationService.recommendModel(true, 4, true, 6));
     }
 
 }
